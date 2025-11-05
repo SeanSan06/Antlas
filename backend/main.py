ae1c96ac821d3b7ceac96f0e90283f3b39b23a49
Daniel Hurtarte <dhurtart@uci.edu>
2025-11-05 00:58:28 -0800

    Create model data
    Replaced item types from string to an item class

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    text: str = None
+    is_done: bool = False
 
 items = []
 
@@ -9,14 +14,17 @@ def root():
     return {"Hello": "World"}
 
 @app.post("/items")
-def create_item(item: str):
+def create_item(item: Item):
     items.append(item)
     return items
 
+@app.get("/items")
+def list_items(limit: int = 10):
+    return items[0:limit]
+
 @app.get("/items/{item_id}")
-def get_item(item_id: int) -> str:
+def get_item(item_id: int) -> Item:
     if item_id < len(items):
         return items[item_id]
     else:
         raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
-
