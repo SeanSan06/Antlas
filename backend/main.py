dda4f4c6721025baea05344a6e47ff72eb0e13f9
Daniel Hurtarte <dhurtart@uci.edu>
2025-11-05 20:39:42 -0800

    Create endpoints
    Create endpoints for creating an event, getting all events, and joining
    an event

--- backend/main.py
+++ backend/main.py
@@ -14,6 +14,24 @@ class Event(BaseModel):
 
 events_db = []
 
-@app.post("/events")
-def create_item(item: Item):
-    pass
+@app.post("/events", response_model=Event)
+def create_item(event: Event):
+    for e in events_db:
+        if e.id == event.id:
+            raise HTTPException(status_code=400, detail="Event with this ID already exists")
+    events_db.append(event)
+
+    return event
+
+@app.get("/events", response_model=List[Event])
+def get_events():
+    return events_db
+
+@app.post("/events/{event_id}/join")
+def join_event(event_id: int, attendee: str):
+    for event in events_db:
+        if event.id == event_id:
+            if attendee not in event.attendees:
+                event.attendees.append(attendee)
+            return event
+    raise HTTPException(status_code=404, detail="Event not found")
