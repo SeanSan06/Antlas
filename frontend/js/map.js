1de21f92ff3c69e9d14d8c58d8686c8e00641102
Cole Saldanha <121999809+elcosas@users.noreply.github.com>
2025-11-06 16:30:59 -0800

    feat: Add psuedocode to map.js

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+function initializeMap() {
   const NE = L.latLng(33.651154, -117.834919);
   const SW = L.latLng(33.641249, -117.850411);
   const bounds = L.latLngBounds(NE, SW);
@@ -14,11 +14,40 @@ document.addEventListener('DOMContentLoaded', function() {
     maxBounds: bounds,
     maxBoundsViscosity: 1.0
   });
-  
+
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
   }).addTo(map);
 
+  return map;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const allEvents = fetch('http://localhost:8000/event');
+
+  // parse events into objects
+  // for each event:
+    // create marker w/ overrided click event
+    // add to array
+  
+  // initialize map
+  const map = initializeMap();
+
+  // for each marker:
+    // add to map
+
+  // handle map events:
+    // on double click:
+      // create marker
+      // invoke popup
+      // if popup form submit:
+        // create new event
+        // override marker w/ click event
+        // close popup
+      // else if click outside:
+        // delete marker
+        // close popup
+  
   map.on('dblclick', function(e) {
     console.log('hi');
     L.marker(e.latlng).addTo(map);
